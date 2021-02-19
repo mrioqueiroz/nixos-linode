032a1537a0e74c4fbde48d7f21569b8331076e19
Mário Queiroz <mario.al.queiroz@gmail.com>
2021-02-19 01:16:25 -0300

    Fix replacements

--- install.sh
+++ install.sh
@@ -10,16 +10,19 @@ mount /dev/sda /mnt
 swapon /dev/sdb
 nixos-generate-config --root /mnt
 
-hardware="/mnt/etc/nixos/hardware-configuration.nix"
+hardware="./hard.nix"
+tmp="tmp.nix"
 
 # FIX:
 sed -i 's/.*fileSystems.*//g' ${hardware}
 sed -i 's/.*device.*//g' ${hardware}
 sed -i 's/.*fsType.*;//g' ${hardware}
 sed -i 's/.*swapDevices.*//g' ${hardware}
-sed -i 's/}\n\s\s\s\s];//g' ${hardware}
 sed -i 's/\s\s};//g' ${hardware}
-sed -i 's/\n//g' ${hardware}
+
+head -n -6 ${hardware} > ${tmp}
+echo "}" >> ${tmp}
+mv ${tmp} ${hardware}
 
 curl -L https://github.com/mrioqueiroz/nixos-linode/raw/main/configuration.nix > /mnt/etc/nixos/configuration.nix
 nixos-install
